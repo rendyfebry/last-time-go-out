a85325b9223e78a891ea44774bf3e68b60046566
Rendy Febry <rendyfebry@gmail.com>
2020-04-15 18:30:01 +0700

    Add Analytics

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 import Vue from "vue";
 import App from "./App.vue";
+import VueAnalytics from "vue-analytics";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
 Vue.config.productionTip = false;
+
+Vue.use(VueAnalytics, {
+  id: 'UA-163723782-1'
+})
 
 new Vue({
   render: h => h(App)
